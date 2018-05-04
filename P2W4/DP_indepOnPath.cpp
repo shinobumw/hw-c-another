da9dd2b69c2fe9fa5fb0435c7fbc47097b95dbe2
shinobumw <shinobumw@users.noreply.github.com>
2018-05-04 12:37:56 +0800

    Update p2w4

---
+++ P2W4/DP_indepOnPath.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while (cin >> n && n) {
+        int d0 = 0, d1 = 0;
+        int tmp0 = 0, tmp1 = 0;
+        while (n--) {
+            int w;
+            cin >> w;
+            tmp0 = max(d0, d1);
+            tmp1 = w + d0;
+            d0 = tmp0;
+            d1 = tmp1;
+        }
+        cout << max(d0, d1) << '\n';
+    }
+    return 0;	
+}
