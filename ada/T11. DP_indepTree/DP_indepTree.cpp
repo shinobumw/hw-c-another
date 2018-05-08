5e8d6d0bfb9f48c73b3e15b9906cee362cdaa730
shinobumw <shinobumw@users.noreply.github.com>
2018-05-08 19:27:04 +0800

    Update ada

--- ada/T11. DP_indepTree/DP_indepTree.cpp
+++ ada/T11. DP_indepTree/DP_indepTree.cpp
@@ -3,44 +3,43 @@ using namespace std;
 
 const int MAX = 1001;
 
-int tf0[MAX], tf1[MAX];
+int dp0[MAX], dp1[MAX];
 int w[MAX];
 
 void sol()
 {
     int n;
-        cin >> n >> w[1];
+    cin >> n >> w[1];
 
-        int parent[MAX], degree[MAX];
-        for (int i = 1; i <= n; ++i)
-            degree[i] = 0;
+    int parent[MAX], degree[MAX];
+    memset(degree, 0, sizeof(degree));
+    for (int i = 2; i <= n; ++i) {
+        cin >> parent[i] >> w[i];
+        degree[parent[i]]++;
+    }
 
-        for (int i = 2; i <= n; ++i) {
-            cin >> parent[i] >> w[i];
-            degree[parent[i]]++;
-        }
+    for (int i = 1; i <= n; ++i) {
+        dp0[i] = 0;
+        dp1[i] = w[i];
+    }
 
-        for (int i = 1; i <= n; ++i)
-            tf0[i] = 0;
-        for (int i = 1; i <= n; ++i)
-            tf1[i] = w[i];
+    queue<int> q;
+    for (int i = 1; i <= n; ++i)
+        if (degree[i] == 0)
+            q.push(i);
 
-        queue<int> q;
-        for (int i = 1; i <= n; ++i)
-            if (degree[i] == 0)
-                q.push(i);
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        int p = parent[v];
+        dp0[p] += max(dp0[v], dp1[v]);
+        dp1[p] += dp0[v];
+        degree[p]--;
+        if (degree[p] == 0)
+            q.push(p);
+    }
 
-        while (!q.empty()) {
-            int v = q.front();
-            q.pop();
-            int p = parent[v];
-            tf0[p] += max(tf0[v], tf1[v]);
-            tf1[p] += tf0[v];
-            degree[p]--;
-            if (degree[p] == 0)
-                q.push(p);
-        }
-        cout << max(tf0[1], tf1[1]) << '\n';
+    cout << max(dp0[1], dp1[1]) << '\n';
 }
 
 int main()
